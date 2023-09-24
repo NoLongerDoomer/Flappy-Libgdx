127873ca02debcef2d821e16c63202add1474795
swarup_home_pc <swarup.rajpurohit.EXT@nmims.edu>
2023-09-24 23:02:36 +0530

    fist commit

---
+++ android/src/com/flappy/libgdx/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.flappy.libgdx;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.flappy.libgdx.FlappyMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyMain(), config);
+	}
+}
